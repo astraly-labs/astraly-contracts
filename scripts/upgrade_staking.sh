47501d815bff54f26d26d6ca1c149f4fac1be074
Evolve <Artevolve@yahoo.com>
2022-06-28 19:40:44 +0200

    Update upgrade_staking.sh

--- scripts/upgrade_staking.sh
+++ scripts/upgrade_staking.sh
@@ -7,10 +7,10 @@ export OWNER_ACCOUNT_NAME=owner
 # export STARKNET_GATEWAY_URL=http://127.0.0.1:5000
 # export STARKNET_FEEDER_GATEWAY_URL=http://127.0.0.1:5000
 
-SALT=0x2
+SALT=0x3
 MAX_FEE=54452800237082000
 
-PROXY_ADDRESS=0x005f4736f8446220048498d4011792ce35b17eca8ff905653b84235ae3d5ef86
+PROXY_ADDRESS=0x005ef67d8c38b82ba699f206bf0db59f1828087a710bad48cc4d51a2b0da4c29
 
 ################################################################################## COMPILE ##########################################################################################
 cd ../
@@ -26,8 +26,8 @@ echo "${ZK_PAD_STAKING_DECLARATION_OUTPUT}"
 ZK_PAD_STAKING_CLASS_HASH=$(awk 'NR==2 {print $4}' <<< "${ZK_PAD_STAKING_DECLARATION_OUTPUT}")
 
 ################################################################################## DEPLOY ##########################################################################################
-echo "Deploy ZkPadStaking"
-starknet deploy --contract ../artifacts/ZkPadStaking.json --salt ${SALT}
+# echo "Deploy ZkPadStaking"
+# starknet deploy --contract ../artifacts/ZkPadStaking.json --salt ${SALT}
 
 
 echo "Upgrade Implementation"
