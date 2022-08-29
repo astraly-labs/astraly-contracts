b445c5bd2e5041c8cfb6173ef0bf745c3dfa54e7
MavericksFive <95299145+MavericksFive@users.noreply.github.com>
2022-08-29 18:49:31 +0200

    add the multithreading option

--- scripts/auto_burn.py
+++ scripts/auto_burn.py
@@ -1,6 +1,6 @@
 import os
-from datetime import datetime, timedelta
-from time import time 
+from datetime import datetime
+import threading
 from time import sleep
 from dotenv import load_dotenv
 from starknet_py.contract import Contract
@@ -14,37 +14,26 @@ from pymongo import MongoClient
 import json
 load_dotenv()
 
-#Set address and key of moderator in .env file
-
-moderator_address = os.getenv('MORDERATOR_ADDRESS')
-moderator_private_key=os.getenv('MORDERATOR_PRIVATE_KEY')
-moderator_public_key=os.getenv('MODERATOR_public_KEY')
-
-lottery_contract_address = os.getenv('LOTTERY_CONTRACT_ADDRESS')
-
 HOST = os.getenv("DB_HOST")
-
-#list of accounts which activate the auto_burn option
-auto_burn_accounts = []
-
-#MUST SPECIFY THE IDO's ID:
-IDO_ID = 0
-
-# Creates an instance of the moderator account which is already deployed (testnet):
-
-client = GatewayClient(TESTNET)
-account_client_testnet = AccountClient(
-    client=client,
-    address= moderator_address,
-    key_pair=KeyPair(private_key=moderator_private_key, public_key=moderator_public_key),
-    chain=StarknetChainId.TESTNET,
-)
-
 mongo_client = MongoClient(HOST)
 db = mongo_client['zkpad-dev']
 history = db.questsHistory
 
-#Get the lottery's contract abi
+ #Lottery Contract Address
+
+lottery_contract_address = os.getenv('LOTTERY_CONTRACT_ADDRESS')
+
+#IDO Factory Contract Address
+
+factory_contract_address = os.getenv('FACTORY_CONTRACT_ADDRESS')
+
+#MUST SPECIFY THE IDO's ID:
+IDO_ID = 0
+
+#list of accounts which activate the auto_burn option
+auto_burn_accounts = [0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc,0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc]
+
+ #Get the lottery's contract abi
 
 with open("./artifacts/abis/AstralyLotteryToken.json") as f:
     info_json = json.load(f)
@@ -54,112 +43,141 @@ lottery_abi = info_json
 
 with open("./artifacts/abis/AstralyIDOFactory.json") as f:
     info_json = json.load(f)
-factory_abi = info_json
+    factory_abi = info_json
 
 #Get the IDO's contract abi
 
-with open("./artifacts/abis/AstralyINOContract.json") as f:
+with open("./artifacts/abis/AstralyIDOContract.json") as f:
     info_json = json.load(f)
-INO_abi = info_json
+IDO_abi = info_json
 
-#lottery contract address
+#set the moderators_numbers
+moderators_numbers = 2
 
-lottery_contract_address = ("0x02d65855cfe2b0d6a8131cc811fb1333d830706539f1eaff4c292fcf4328dc27")
+def autoBurn(moderator_ID):
 
-#IDO Factory Contract Address
+    sublists_len = int(len(auto_burn_accounts) / moderators_numbers)
 
-factory_contract_address = ("0x0025fc12d7afb01f6ad82a793737b2c7908fc8d6fcd80bd55abf82eb38de9596")
+    if moderator_ID < (moderators_numbers - 1):
+        auto_burn_accounts_selected = auto_burn_accounts[sublists_len*moderator_ID:sublists_len*(moderator_ID+1)]
+    else:
+        auto_burn_accounts_selected = auto_burn_accounts[sublists_len*moderator_ID:len(auto_burn_accounts)]
 
-#create the factory contract
-factory_contract = Contract(
-    factory_contract_address,
-    factory_abi,
-    account_client_testnet,
-)
+    #Set address and key of moderators in .env file
 
-#create the lottery contract
-lottery_contract = Contract(
-    lottery_contract_address,
-    lottery_abi,
-    account_client_testnet,
-)
-
-#get the ido_contract_address
-
-ido_address = hex(factory_contract.functions["get_ido_address"].call_sync(IDO_ID).address)
-
-#create the ido contract
-
-IDO_contract = Contract(
-    ido_address,
-    INO_abi,
-    account_client_testnet,
-)
-
-#get the IDO contract registration start date
-
-registration_start = IDO_contract.functions["get_registration"].call_sync().res['registration_time_starts']
-now = datetime.today().timestamp()
-time_until_registration_start = registration_start - now
-
-#wait until the registration has started
-if (time_until_registration_start > 0):
-    sleep(registration_start - now)
-
-#create a call list to integrate the multicall option
-calls = []
-
-#get the number of multicalls (depends on the number of transactions you want)
-number_calls_per_multicall = 55
-number_auto_burn_users = len(auto_burn_accounts)
-
-#number of multicalls -1
-number_multicalls = (number_auto_burn_users) // number_calls_per_multicall
-
-iterator = 0
-last_multicall_iterator = 0
-
-#get the merkle_proof
-
-merkle_proof = db.merkleProofs.find_one({"idoId":IDO_ID})
-
-for address in auto_burn_accounts:
-    iterator = iterator + 1
-    print(iterator)
-
-    #get the number of quests
-
-    nb_Quest = history.count_documents({"address": address, "idoId": IDO_ID})
-
-    #get the amount of tickets to burn per users
-
-    amount = lottery_contract.functions["balanceOf"].call_sync(address, IDO_ID).balance
-
-    #check if the amount is  greater than 0
-    if amount>0:
-        
-    #add call to the calls list (function burn because no quest done)
-        if (nb_Quest == 0):
-            calls.append(lottery_contract.functions["burn"].prepare(address, IDO_ID, amount))
-
-        if (nb_Quest > 0):
-            calls.append(lottery_contract.functions["burn_with_quest"].prepare(address, IDO_ID, amount, nb_Quest, merkle_proof))
-
-    #execute a multicall after iterator reachs the number_calls_per_multicall's value
-
-        if ((number_multicalls > 0) and ((iterator % number_calls_per_multicall) == 0)):
-            burn_tickets = account_client_testnet.execute_sync(calls=calls, max_fee=int(1e16))
-            account_client_testnet.wait_for_tx_sync(burn_tickets.transaction_hash)
-            print(burn_tickets)
-            calls.clear()
-
-    #last multicall
-        if (iterator == number_auto_burn_users):
-            burn_tickets = account_client_testnet.execute_sync(calls=calls, max_fee=int(1e16))
-            account_client_testnet.wait_for_tx_sync(burn_tickets.transaction_hash)
-            print(burn_tickets)
+    moderator_address = os.getenv(f'MODERATOR_ADDRESS_{moderator_ID}')
+    moderator_private_key=os.getenv(f'MODERATOR_PRIVATE_KEY_{moderator_ID}')
+    moderator_public_key=os.getenv(f'MODERATOR_PUBLIC_KEY_{moderator_ID}')
 
 
+    # Creates an instance of the moderator account which is already deployed (testnet):
 
+    client = GatewayClient(TESTNET)
+
+    account_client_testnet = AccountClient(
+        client=client,
+        address= moderator_address,
+        key_pair=KeyPair(private_key=int(moderator_private_key,16), public_key=int(moderator_public_key,16)),
+        chain=StarknetChainId.TESTNET,
+    )
+
+    #create the lottery contract
+    lottery_contract = Contract(
+        lottery_contract_address,
+        lottery_abi,
+        account_client_testnet,
+    )
+
+    #create the factory contract
+    factory_contract = Contract(
+        factory_contract_address,
+        factory_abi,
+        account_client_testnet,
+    )
+
+    #get the ido_contract_address
+
+    ido_address = hex(factory_contract.functions["get_ido_address"].call_sync(IDO_ID).address)
+
+    #create the ido contract
+
+    IDO_contract = Contract(
+        ido_address,
+        IDO_abi,
+        account_client_testnet,
+    )
+
+    #get the IDO contract registration start date
+
+    registration_start = IDO_contract.functions["get_registration"].call_sync().res['registration_time_starts']
+    now = datetime.today().timestamp()
+    time_until_registration_start = registration_start - now
+
+    #wait until the registration has started
+    if (time_until_registration_start > 0):
+        sleep(registration_start - now)
+
+    #create a call list to integrate the multicall option
+    calls = []
+
+    #get the number of multicalls (depends on the number of transactions you want)
+    number_calls_per_multicall = 3
+    number_auto_burn_users = len(auto_burn_accounts_selected)
+
+    #number of multicalls -1
+    number_multicalls = (number_auto_burn_users) // number_calls_per_multicall
+
+    iterator = 0
+
+    #get the merkle_proof
+
+    merkle_proof = db.merkleProofs.find_one({"idoId":IDO_ID})
+
+    for address in auto_burn_accounts_selected:
+        iterator = iterator + 1
+        print(iterator)
+
+        #get the number of quests
+
+        nb_Quest = history.count_documents({"address": address, "idoId": IDO_ID})
+
+        #get the amount of tickets to burn per users
+
+        amount = lottery_contract.functions["balanceOf"].call_sync(address, IDO_ID).balance
+
+        #check if the amount is  greater than 0
+        try:
+            if amount>0:
+
+            #add call to the calls list (function burn because no quest done)
+                if (nb_Quest == 0):
+                    calls.append(lottery_contract.functions["burn"].prepare(address, IDO_ID, 1))
+
+                if (nb_Quest > 0):
+                    calls.append(lottery_contract.functions["burn_with_quest"].prepare(address, IDO_ID, amount, nb_Quest, merkle_proof[address]))
+
+            #execute a multicall after iterator reachs the number_calls_per_multicall's value
+
+                    if ((number_multicalls > 0) and ((iterator % number_calls_per_multicall) == 0)):
+                        burn_tickets = account_client_testnet.execute_sync(calls=calls, max_fee=int(1e16))
+                        print(burn_tickets)
+                        account_client_testnet.wait_for_tx_sync(burn_tickets.transaction_hash)
+                        print(burn_tickets)
+                        calls.clear()
+
+            #last multicall
+
+                    if (iterator == number_auto_burn_users):
+                        burn_tickets = account_client_testnet.execute_sync(calls=calls, max_fee=int(1e16))
+                        account_client_testnet.wait_for_tx_sync(burn_tickets.transaction_hash)
+                        print(burn_tickets)
+        except Exception as error:
+            print(f"TRANSACTION ERROR: {error}")
+
+#multithreading
+
+for i in range(moderators_numbers):
+    moderator_thread = threading.Thread(target=autoBurn, args=(i,))
+    moderator_thread.start()
 
 
