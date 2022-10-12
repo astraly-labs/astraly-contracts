c8b55d1f722d9a94bb96bd2b912040088f5a06dd
Filip Laurentiu <f.laurentiu26@gmail.com>
2022-10-12 13:30:39 +0300

    Fix error message

--- tests/test_ido_contract.py
+++ tests/test_ido_contract.py
@@ -534,7 +534,7 @@ async def test_only_admin_can_setup_sale(contracts_factory):
                 *BASE_ALLOCATION
             ],
         ),
-        reverted_with=f"AccessControl: caller is missing role {str_to_felt('OWNER')}",
+        reverted_with=f"AccessControl::Caller is missing role {str_to_felt('OWNER')}",
     )
 
 
