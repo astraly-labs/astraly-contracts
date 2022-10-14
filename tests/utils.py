86c43b250db9c1c4227c26a6d38d0ce8d2903aca
Evolve <Artevolve@yahoo.com>
2022-10-14 15:20:48 +0200

    fix: remove comment ✍️

--- tests/utils.py
+++ tests/utils.py
@@ -70,7 +70,6 @@ def assert_events_emitted(tx_exec_info: TransactionExecutionInfo, events):
         )
 
         base = tx_exec_info.call_info.internal_calls[0]
-        # print(base.events, event_obj)
         if event_obj in base.events and from_address == base.contract_address:
             return
 
